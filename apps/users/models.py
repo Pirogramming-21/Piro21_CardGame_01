827e9b3b7abb7702b47d1b4ad3efe697cfd1ab84
Jihun Jeon <jihunee01@gmail.com>
2024-07-18 17:04:42 +0900

    [ADD]setting

---
+++ apps/users/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    first_name = None
+    last_name = None
+    user_name = models.CharField(max_length=10, unique=True)
+    user_score = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.username
---
+++ apps/users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+app_name = 'users'
+
+urlpatterns = [
+#   path('/users', login, name='login'), 예시임
+]
