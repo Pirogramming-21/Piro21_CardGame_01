325a088ff1d3b646a411775d0c37797e44201577
Jihun Jeon <jihunee01@gmail.com>
2024-07-19 01:41:36 +0900

    [ADD]revenge 관련 템플릿, 뷰 추가

--- apps/game/urls.py
+++ apps/game/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 app_name = 'game'
 
 urlpatterns = [
 #   path('', main, name='main'),  예시임
+    path('revenge/<int:pk>', revenge, name='revenge'),
+    path('detail_revenge/<int:pk>', detail_revenge, name='detail_revenge')
+
 ]
--- apps/game/views.py
+++ apps/game/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Game
+from users.models import User
 
 # Create your views here.
+def findWinner(game, user):
+    user_winner = 1 # 0은 패배, 1은 비김, 2는 승리
+    user_score = 0  # 따거나 잃은 점수
+    if game.bigorsmall == True: # 큰 수가 이김
+        if game.attacker_card > game.revenger_card:
+            game.attacker.user_score += game.attacker_card
+            game.revenger.user_score -= game.revenger_card
+            if game.attacker == user:
+                user_winner = 2
+                user_score += game.attacker_card
+            else:
+                user_winner = 0
+                user_score -= game.revenger_card
+        elif game.attacker_card < game.revenger_card:
+                game.attacker.user_score -= game.attacker_card
+                game.revenger.user_score += game.revenger_card
+                if game.attacker == user:
+                    user_winner = 0
+                    user_score -= game.attacker_card
+                else:
+                    user_winner = 2
+                    user_score += game.revenger_card
+    else:
+        if game.attacker_card > game.revenger_card:
+            game.attacker.user_score -= game.attacker_card
+            game.revenger.user_score += game.revenger_card
+            if game.attacker == user:
+                user_winner = 0
+                user_score -= game.attacker_card
+            else:
+                user_winner = 2
+                user_score += game.revenger_card
+        elif game.attacker_card < game.revenger_card:
+                game.attacker.user_score += game.attacker_card
+                game.revenger.user_score -= game.revenger_card
+                if game.attacker == user:
+                    user_winner = 2
+                    user_score += game.attacker_card
+                else:
+                    user_winner = 0
+                    user_score -= game.revenger_card
+    game.attacker.save()
+    game.revenger.save()
+    return user_winner, user_score
+
+def revenge(req, pk):
+    game = Game.objects.get(id=pk)
+    if req.method == "GET":
+        form = revengeForm(request = req, instance = game)
+        ctx = {'form': form, 'pk':pk}
+        return render(req, 'game/revenge.html', ctx)
+    
+    form = revengeForm(req.POST, instance = game)
+    if form.is_valid():
+            game = form.save()
+            user_result, user_score = findWinner(game, req)
+            ctx = {'game': game, 'user_result':user_result, 'user_score':user_score}
+    return render(req, 'game/detail_result.html', ctx)
+
+def detail_revenge(req, pk):
+    game = Game.objects.get(id=pk)
+    ctx = {'game': game}
+    return render(req, 'game/detail_revenge.html', ctx)
