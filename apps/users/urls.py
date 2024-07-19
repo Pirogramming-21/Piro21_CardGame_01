c8f42bc1c8e8bedd2b2dacc819b386281ba23847
Jihun Jeon <jihunee01@gmail.com>
2024-07-19 14:12:21 +0900

    [ADD] users template추가

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 app_name = 'users'
 
 urlpatterns = [
-#   path('/users', login, name='login'), 예시임
+    path('',main, name="main"),
+    path('login/', login, name="login"),
+    path('logout/', logout, name="logout"),
+    path('signup/', signup, name="singup"),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def main(req):
+    pass
+
+def signup(req):
+    pass
+
+def login(req):
+    pass
+
+def logout(req):
+    pass
