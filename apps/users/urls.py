40030c3f637e41899339554b9bf75dd5c119eef9
Handher0 <handher0@naver.com>
2024-07-19 14:20:11 +0900

    [ADD] 영웅 뷰 추가

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,5 +4,9 @@ from .views import *
 app_name = 'users'
 
 urlpatterns = [
+    path('',main, name="main"),
+    path('login/', login, name="login"),
+    path('logout/', logout, name="logout"),
+    path('signup/', signup, name="singup"),
 #   path('/users', login, name='login'), 예시임
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from users.forms import SignupForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 # Create your views here.
+
+def main(req):
+    return render(req, "users/main.html")
+
+def login(req):
+    if req.method == 'POST':
+        form = AuthenticationForm(req, req.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth.login(req, user)
+            return redirect('users:main')
+        else:
+            context = {
+                'form':form,
+            }
+            return render(req,template_name='users/login.html',context=context)
+    else:
+        form = AuthenticationForm()
+        context = {
+            'form': form,
+        }
+        return render(req, template_name='users/login.html', context=context)
+
+def logout(req):
+    auth.logout(req)
+    return redirect('users:main')
+
+def signup(req):
+    pass
