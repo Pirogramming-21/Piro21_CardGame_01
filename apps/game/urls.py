83cc04b36d4d3d35ca81d19e30e58e2b4cebc6aa
Jihun Jeon <jihunee01@gmail.com>
2024-07-20 09:01:49 +0900

    main 초기화

--- apps/game/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = 'game'
-
-urlpatterns = [
-#   path('', main, name='main'),  예시임
-]
--- apps/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-app_name = 'users'
-
-urlpatterns = [
-#   path('/users', login, name='login'), 예시임
-]
